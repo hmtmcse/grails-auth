6e5b652481836d46fc07af941c9f3794ca3b5de7
Touhid Mia <hmtm.cse@gmail.com>
2019-09-05 00:27:10 +0600

    Test Class

---
+++ src/test/groovy/com/hmtmcse/gauth/UserAccessListSpec.groovy
@@ -0,0 +1,18 @@
+package com.hmtmcse.gauth
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class UserAccessListSpec extends Specification implements DomainUnitTest<UserAccessList> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
