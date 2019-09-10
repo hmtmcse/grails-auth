2fde5ca9cb3f7da9d20f39bbbcef8559e5b42b30
Touhid Mia <hmtm.cse@gmail.com>
2019-09-11 03:04:24 +0600

    Migrated to v2

---
+++ src/test/groovy/com/hmtmcse/gauth/api/ApiAuthenticationV1ControllerSpec.groovy
@@ -0,0 +1,18 @@
+package com.hmtmcse.gauth.api
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class ApiAuthenticationV1ControllerSpec extends Specification implements ControllerUnitTest<ApiAuthenticationV1Controller> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
