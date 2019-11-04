489aa6114cbcb450d431653e613b40a3eb207bd6
Touhid Mia <hmtm.cse@gmail.com>
2019-11-04 07:09:24 +0600

    Documentation

--- src/test/groovy/com/hmtmcse/gauth/api/ApiUserV1ControllerSpec.groovy
+++ src/test/groovy/com/hmtmcse/gauth/api/ApiUserV1ControllerSpec.groovy
@@ -1,5 +1,7 @@
 package com.hmtmcse.gauth.api
 
+import com.hmtmcse.gauth.definition.UserDefinitionService
+import com.hmtmcse.gs.GsRestfulService
 import grails.testing.web.controllers.ControllerUnitTest
 import spock.lang.Specification
 
@@ -11,8 +13,17 @@ class ApiUserV1ControllerSpec extends Specification implements ControllerUnitTes
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test postCreate Action"() {
+
+        given:
+        controller.userDefinitionService = Stub(UserDefinitionService)
+        controller.gsRestfulService = Stub(GsRestfulService)
+
+
+        when: "The message action is invoked"
+        controller.postCreate()
+
+        then: "Hello is returned"
+        response.text == 'Hello'
     }
 }
