cbe9baaee9565be5796c2daeb2f88b5386f00733
Touhid Mia <hmtm.cse@gmail.com>
2019-12-28 19:10:10 +0600

    Grails 786

--- grails-app/services/com/hmtmcse/gauth/AclService.groovy
+++ grails-app/services/com/hmtmcse/gauth/AclService.groovy
@@ -1,10 +1,21 @@
 package com.hmtmcse.gauth
 
+import grails.gorm.transactions.Transactional
+
 
 class AclService {
 
-    def serviceMethod() {
-
+    @Transactional
+    def addAcl(List listOfMapAcl, String groupName = AuthConstant.GROUP_ADMINISTRATOR) {
+        UserAccess userAccess
+        UserAccessGroup userAccessGroup = UserAccessGroup.findByIdentifier(groupName)
+        listOfMapAcl.each { access ->
+            userAccess = new UserAccess(access)
+            userAccess.isAllowedAllAction = true
+            userAccess.userAccessGroup = userAccessGroup
+            userAccess.actionName = [:]
+            userAccess.save()
+        }
     }
 
 }
